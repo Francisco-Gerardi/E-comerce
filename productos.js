eba95f15fc596a99883d3c9f3106520a1ff51e5e
Francisco <gerardifranciscojavier@gmail.com>
2024-10-29 20:27:18 -0300

    Commit inicial

---
+++ productos.js
@@ -0,0 +1,20 @@
+//productos
+
+let stockProductos =[
+    {id: 1, nombre: "zapa 1", estilo: "sport",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa1.png"},
+    {id: 2, nombre: "zapa 2", estilo: "sport",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa2.png"},
+    {id: 3, nombre: "zapa 3", estilo: "sport",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa3.png"},
+    {id: 4, nombre: "zapa 4", estilo: "sport",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa4.png"},
+    {id: 5, nombre: "zapa 5", estilo: "urban",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa5.png"},
+    {id: 6, nombre: "zapa 6", estilo: "urban",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa6.png"},
+    {id: 7, nombre: "zapa 7", estilo: "urban",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa7.png"},
+    {id: 8, nombre: "zapa 8", estilo: "urban",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa8.png"},
+    {id: 9, nombre: "zapa 9", estilo: "lower",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa9.png"},
+    {id: 10, nombre: "zapa 10", estilo: "lower",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa10.png"},
+    {id: 11, nombre: "zapa 11", estilo: "lower",   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa11.png"},
+    {id: 12, nombre: "zapa 12", estilo: "lower",   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa12.png"},
+    {id: 13, nombre: "zapa 13", estilo: "jordan" ,   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa13.png"},
+    {id: 14, nombre: "zapa 14", estilo: "jordan" ,   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa14.png"},
+    {id: 15, nombre: "zapa 15", estilo: "jordan" ,   precio: 1200, talles: "37 38 39 40 41 42", img: "imagenes/zapa15.png"},
+    {id: 16, nombre: "zapa 16", estilo: "jordan" ,   precio: 1300, talles: "37 38 39 40 41 42", img: "imagenes/zapa16.png"},
+]
